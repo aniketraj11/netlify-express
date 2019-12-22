4fc317166a306d26051416096efa167b45fe0bfe
aniketraj11 <aniketraj11@gmail.com>
2019-12-23 00:09:01 +0530

    Update server.js

--- express/server.js
+++ express/server.js
@@ -1,4 +1,4 @@
-/*
+
 'use strict';
 const express = require('express');
 const path = require('path');
@@ -57,58 +57,3 @@ app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
 module.exports = app;
 module.exports.handler = serverless(app);
-*/
-var express = require("express");
-var app = express();
-var server = require("http").createServer(app);
-var cors = require("cors");
-
-app.use(cors());
-
-var flag = 0;
-
-var data = [
-    { sunrise: "05:02:00", 
-     sunset: "17:02:00", 
-     moonrise: "18:02:00", 
-     Moonset: "04:02:00", 
-     tithi: "Shukla Dashmi",
-     nakshatra: "Anuradha",
-     yog: "Endra",
-     hindu_maah: "Shraavan",
-     paksha: "Shukla Paksha", },
-    { sunrise: "06:02:00", 
-     sunset: "18:02:00", 
-     moonrise: "19:02:00", 
-     Moonset: "05:02:00", 
-     tithi: "Krishna Paksha",
-     nakshatra: "Swati",
-     yog: "Atiganda",
-     hindu_maah: "Shraavan",
-     paksha: "Shukla Paksha", }
-]
-
-
-app.get("/", function(req, res, next) {
-    res.setHeader("Content-Type", "application/json");
-    res.end(JSON.stringify({ data }));
-});
-
-app.get("/panchang", function(req, res, next) {
-    let index = 0;
-    if(flag == 0){
-        index = 0;
-        flag = 1;
-    }
-    else{
-        index = 1;
-        flag = 0;
-    }
-    res.setHeader("Content-Type", "application/json");
-    res.end(JSON.stringify(data[index]));
-});
-
-
-server.listen(6060, function() {
-    console.log("listening on *:6060");
-});
