6d13aa5fd750a29de1ed306faf3ef5b46514bbdb
aniketraj11 <aniketraj11@gmail.com>
2019-12-22 23:45:55 +0530

    Update server.js

--- express/server.js
+++ express/server.js
@@ -49,7 +49,7 @@ router.get('/', (req, res) => {
 
 
 
-app.use(bodyParser.json());
+//app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
 //app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
