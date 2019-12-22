4132210cf67fac9527280926b66207ac8961a6c2
aniketraj11 <aniketraj11@gmail.com>
2019-12-22 23:29:02 +0530

    Update server.js

--- express/server.js
+++ express/server.js
@@ -44,14 +44,14 @@ router.get('/', (req, res) => {
     res.setHeader("Content-Type", "application/json");
     res.end(JSON.stringify(data[index]));
 });
-/*
-router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-router.post('/', (req, res) => res.json({ postBody: req.body }));
+
+
+
 
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
 app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
-*/
+
 
 module.exports = app;
 module.exports.handler = serverless(app);
